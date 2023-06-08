221b78909382424a14035fcb078e8d99e9037687
ABDI NEGARA GUCI <102046045+abdinegara783@users.noreply.github.com>
2023-06-09 02:34:33 +0700

    Update Test_Dicoding.py

--- App/Test_Dicoding.py
+++ App/Test_Dicoding.py
@@ -1,5 +1,40 @@
+from flask import Flask, jsonify, request
 from dicoding import platform
-Learning_path = 'Android'
-need = 'saya ingin mencari mentor yang ahli dalam bidang Android'
-experience_level='intermediate'
-platform.match_mentee_with_mentor(name, need, experience_level)
+
+app = Flask(__name__)
+
+@app.route('/matchmaking', methods=['POST'])
+def matchmaking():
+    data = request.get_json()
+    Learning_path = data['Learning_path']
+    need = data['need']
+    
+    mentors = platform.match_mentee_with_mentor(Learning_path, need)
+    
+    if len(mentors) > 0:
+        mentor_list = [{
+            "id": mentor.mentor_id,
+            "photoProfile": mentor.pictures,
+            "fullName": mentor.name,
+            "job": mentor.jobs,
+            "averageRating": mentor.rating
+        } for mentor, _ in mentors]
+        
+        response = {
+            "status": "success",
+            "data": {
+                "mentors": mentor_list
+            }
+        }
+    else:
+        response = {
+            "status": "success",
+            "data": {
+                "mentors": []
+            }
+        }
+    
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run()
